b4f30a10eb44be1ec9b0a490ba569c774c971e62
eunbinnie <dmsqlsdl54@gmail.com>
2025-10-28 14:16:30 +0900

    fix: globals.css 파일 import 오류 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
-import '@/app/styles/globals.css';
-
 import type { Metadata } from 'next';
 
 import { pretendard } from '@/lib/fonts';
+
+import '@/styles/globals.css';
 
 export const metadata: Metadata = {
   title: 'Whisbeen',
