98d08c1cba8b81641749a646111247c845a3588d
eunbinnie <dmsqlsdl54@gmail.com>
2025-10-29 10:45:56 +0900

    feat: 회원가입 페이지 레이아웃 세팅

--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -1,5 +1,29 @@
+import Link from 'next/link';
+
 const SignupPage = () => {
-  return <div>회원가입 전체 페이지</div>;
+  return (
+    <div className="flex h-full min-h-dvh">
+      <div className="hidden flex-1 md:block">
+        <div className="h-full bg-muted p-10">
+          <Link href={'/'} className="text-lg font-medium">
+            Whisbeen
+          </Link>
+        </div>
+      </div>
+      <div className="flex-1">
+        <div className="flex h-full flex-col p-8">
+          <Link href={'/login'} className="ml-auto px-4 py-2 text-sm">
+            로그인
+          </Link>
+          <div className="flex-1 bg-red-200">
+            {/* TODO: 회원가입 폼 추가 */}
+          </div>
+          {/* 회원가입 폼 중앙 정렬 위해 div 추가 */}
+          <div className="h-8" />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default SignupPage;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,10 @@
+import Link from 'next/link';
+
 export default function Home() {
-  return <div>Hello World</div>;
+  return (
+    <div>
+      Hello World
+      <Link href="/signup">회원가입</Link>
+    </div>
+  );
 }
